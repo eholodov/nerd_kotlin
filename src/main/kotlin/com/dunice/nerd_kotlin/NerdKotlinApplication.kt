648c3692b65cbaab350af680d4c1e2d23ff4bebc
Violetta Sytnikova <v.sytnikova@dunice.net>
2021-09-08 14:49:21 +0300

    Initial commit

---
+++ src/main/kotlin/com/dunice/nerd_kotlin/NerdKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.dunice.nerd_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NerdKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<NerdKotlinApplication>(*args)
+}
