a17348fc42dcace58defc69386f97e5c57034cdc
Violetta Sytnikova <v.sytnikova@dunice.net>
2021-09-13 11:10:17 +0300

    made refactoring of the function

--- src/main/kotlin/com/dunice/nerd_kotlin/common/utils/DateTimeUtils.kt
+++ src/main/kotlin/com/dunice/nerd_kotlin/common/utils/DateTimeUtils.kt
@@ -7,8 +7,6 @@ import java.time.temporal.ChronoField
 @Component
 class DateTimeUtils {
 
-    fun getNumberOfWeek(): Int {
-        var date = LocalDateTime.now()
-        return date.get(ChronoField.ALIGNED_WEEK_OF_YEAR)
-    }
+    fun getNumberOfWeek(): Int = LocalDateTime.now().get(ChronoField.ALIGNED_WEEK_OF_YEAR)
+
 }
