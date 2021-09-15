ed010396bd59c8c8019fdaa5b56e0c9f3aaa468c
Violetta Sytnikova <v.sytnikova@dunice.net>
2021-09-15 14:36:07 +0300

    changed whole function to one groupBy

--- src/main/kotlin/com/dunice/nerd_kotlin/common/utils/GroupingUtils.kt
+++ src/main/kotlin/com/dunice/nerd_kotlin/common/utils/GroupingUtils.kt
@@ -6,16 +6,6 @@ import org.springframework.stereotype.Component
 @Component
 class GroupingUtils {
 
-    fun groupCardsByPerson(cards: List<SpreadSheetCardInfo>): MutableMap<String, MutableList<SpreadSheetCardInfo>> {
-        val groupedList = emptyMap<String, MutableList<SpreadSheetCardInfo>>().toMutableMap()
-        for (card in cards) {
-            if (groupedList.containsKey(card.nameStudent)){
-                groupedList[card.nameStudent]?.add(card)
-            }
-            else {
-                groupedList[card.nameStudent] = mutableListOf(card)
-            }
-        }
-        return groupedList
-    }
+    fun groupCardsByPerson(cards: List<SpreadSheetCardInfo>): Map<String, List<SpreadSheetCardInfo>>
+        = cards.groupBy(SpreadSheetCardInfo::nameStudent)
 }
