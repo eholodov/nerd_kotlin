c2f975c5bc97ab21dc88b762455ce38c07c07105
vsytnikova <84845358+vsytnikova@users.noreply.github.com>
2021-09-15 15:49:59 +0300

    Feature/validate group list (#9)
    * Initial commit for grouping cards by person
    
    * fixed if/else condition
    
    * replaced list in values to CardInfo
    
    * returned if/else and changed one card to list of cards
    
    * changed whole function to one groupBy

---
+++ src/main/kotlin/com/dunice/nerd_kotlin/common/utils/GroupingUtils.kt
@@ -0,0 +1,11 @@
+package com.dunice.nerd_kotlin.common.utils
+
+import com.dunice.nerd_kotlin.common.types.SpreadSheetCardInfo
+import org.springframework.stereotype.Component
+
+@Component
+class GroupingUtils {
+
+    fun groupCardsByPerson(cards: List<SpreadSheetCardInfo>): Map<String, List<SpreadSheetCardInfo>>
+        = cards.groupBy(SpreadSheetCardInfo::nameStudent)
+}
