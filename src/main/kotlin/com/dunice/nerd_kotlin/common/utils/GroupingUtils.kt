5c8a7b1f504f7fdb4c9a3d30c5b4fd58ae199170
Violetta Sytnikova <v.sytnikova@dunice.net>
2021-09-16 17:30:25 +0300

    Trying to figure out how to store entities the right way

--- src/main/kotlin/com/dunice/nerd_kotlin/common/utils/GroupingUtils.kt
+++ src/main/kotlin/com/dunice/nerd_kotlin/common/utils/GroupingUtils.kt
@@ -1,5 +1,6 @@
 package com.dunice.nerd_kotlin.common.utils
 
+import com.dunice.nerd_kotlin.common.types.InterviewerCardInfo
 import com.dunice.nerd_kotlin.common.types.SpreadSheetCardInfo
 import org.springframework.stereotype.Component
 
@@ -11,6 +12,16 @@ class GroupingUtils {
 
     fun groupCardsByInterviewerAndAssistant(cards: List<SpreadSheetCardInfo>)  {
         val cardsGroupedByInterviewer = cards.groupBy(SpreadSheetCardInfo::interviewer)
+        val cardsGroupedByInterviewerAndAssistant = emptyMap<String, MutableList<InterviewerCardInfo>>()
+        for (card in cards) {
+            val interviewerCardInfo = InterviewerCardInfo(card)
+            interviewerCardInfo.isAssistant = false
+            if (!card.assistant.isNullOrBlank()) {
+                if (cardsGroupedByInterviewerAndAssistant.containsKey(card.assistant)) {
+                    //cardsGroupedByInterviewerAndAssistant[card.assistant]?.add()
+                }
+            }
+        }
         println()
 
     }
