4cacb6c92b655c27be1835606d7325ed2e3bd187
vsytnikova <84845358+vsytnikova@users.noreply.github.com>
2021-09-20 12:07:43 +0300

    Changed component util functions to "static" (#11)

--- src/main/kotlin/com/dunice/nerd_kotlin/common/utils/DateTimeUtils.kt
+++ src/main/kotlin/com/dunice/nerd_kotlin/common/utils/DateTimeUtils.kt
@@ -4,9 +4,4 @@ import org.springframework.stereotype.Component
 import java.time.LocalDateTime
 import java.time.temporal.ChronoField
 
-@Component
-class DateTimeUtils {
-
-    fun getNumberOfWeek(): Int = LocalDateTime.now().get(ChronoField.ALIGNED_WEEK_OF_YEAR)
-
-}
+fun getNumberOfWeek(): Int = LocalDateTime.now().get(ChronoField.ALIGNED_WEEK_OF_YEAR)
