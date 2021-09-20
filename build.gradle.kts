93c9fedb9d13b3df8138844bb399f1e152a1abbe
eholodov <e.holodov@dunice.net>
2021-09-20 12:18:34 +0300

    Added MongoConfiguration.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,10 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter:2.5.4")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.4")
 
 
 	// https://mvnrepository.com/artifact/org.springframework.data/spring-data-mongodb
@@ -29,22 +29,30 @@ dependencies {
 	implementation("com.slack.api:slack-api-client:1.12.0")
 
 	// https://mvnrepository.com/artifact/com.google.apis/google-api-services-sheets
-	implementation("com.google.apis:google-api-services-sheets:v4-rev612-1.25.0")
+	implementation("com.google.apis:google-api-services-sheets:v4-rev20210629-1.32.1")
 
 	// https://mvnrepository.com/artifact/com.google.api-client/google-api-client
 	implementation("com.google.api-client:google-api-client:1.32.1")
 
-
-	// https://mvnrepository.com/artifact/com.google.api-client/google-api-client-jackson2
-	implementation("com.google.api-client:google-api-client-jackson2:1.20.0")
-
 	// https://mvnrepository.com/artifact/com.google.oauth-client/google-oauth-client-jetty
-	implementation("com.google.oauth-client:google-oauth-client-jetty:1.30.6")
+	implementation("com.google.oauth-client:google-oauth-client-jetty:1.32.1")
 
 	// https://mvnrepository.com/artifact/com.google.auth/google-auth-library-oauth2-http
 	implementation("com.google.auth:google-auth-library-oauth2-http:1.1.0")
 
+	// https://mvnrepository.com/artifact/org.springframework.data/spring-data-mongodb
+	implementation("org.springframework.data:spring-data-mongodb:3.2.4")
 
+	// https://mvnrepository.com/artifact/org.mongodb/mongodb-driver-sync
+	implementation("org.mongodb:mongodb-driver-sync")
+
+
+	constraints {
+		// https://mvnrepository.com/artifact/com.google.api-client/google-api-client-jackson2
+		implementation("com.google.api-client:google-api-client-jackson2:1.20.0") {
+			because("Version 1.31.2 has not com.google.api.client.json.jackson2 package")
+		}
+	}
 
 }
 
