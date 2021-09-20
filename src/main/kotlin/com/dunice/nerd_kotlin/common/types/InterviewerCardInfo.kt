df5cac1c6d483df606eced23a69cf84b43b4344e
Violetta Sytnikova <v.sytnikova@dunice.net>
2021-09-20 11:06:01 +0300

    Fixed issues from PR

--- src/main/kotlin/com/dunice/nerd_kotlin/common/types/InterviewerCardInfo.kt
+++ src/main/kotlin/com/dunice/nerd_kotlin/common/types/InterviewerCardInfo.kt
@@ -1,25 +1,15 @@
 package com.dunice.nerd_kotlin.common.types
 
-class InterviewerCardInfo {
-
-    lateinit var department: String
-    lateinit var date: String
-    lateinit var time: String
-    lateinit var nameStudent: String
-    lateinit var subject: String
-    lateinit var room: String
-    var isAssistant: Boolean = false
-    lateinit var mainInterviewer: String
+data class InterviewerCardInfo (
+    val department: String,
+    val date: String,
+    val time: String,
+    val nameStudent: String,
+    val subject: String,
+    val room: String,
+    var isAssistant: Boolean = false,
+    var mainInterviewer: String
+        ) {
 
 }
 
-fun convertFromSheetCard(cardInfo: SpreadSheetCardInfo): InterviewerCardInfo {
-    val interviewerCardInfo = InterviewerCardInfo()
-    interviewerCardInfo.department = cardInfo.department
-    interviewerCardInfo.date = cardInfo.date
-    interviewerCardInfo.room = cardInfo.room
-    interviewerCardInfo.time = cardInfo.time
-    interviewerCardInfo.nameStudent = cardInfo.nameStudent
-    interviewerCardInfo.subject = cardInfo.subject
-    return interviewerCardInfo
-}
--- src/main/kotlin/com/dunice/nerd_kotlin/common/utils/GroupingUtils.kt
+++ src/main/kotlin/com/dunice/nerd_kotlin/common/utils/GroupingUtils.kt
@@ -2,7 +2,6 @@ package com.dunice.nerd_kotlin.common.utils
 
 import com.dunice.nerd_kotlin.common.types.InterviewerCardInfo
 import com.dunice.nerd_kotlin.common.types.SpreadSheetCardInfo
-import com.dunice.nerd_kotlin.common.types.convertFromSheetCard
 import org.springframework.stereotype.Component
 
 @Component
@@ -11,16 +10,17 @@ class GroupingUtils {
     fun groupCardsByPerson(cards: List<SpreadSheetCardInfo>): Map<String, List<SpreadSheetCardInfo>>
         = cards.groupBy(SpreadSheetCardInfo::nameStudent)
 
-    fun groupCardsByInterviewerAndAssistant(cards: List<SpreadSheetCardInfo>)  {
+    fun groupCardsByInterviewerAndAssistant(cards: List<SpreadSheetCardInfo>): MutableMap<String, MutableList<InterviewerCardInfo>> {
         val cardsGroupedByInterviewerAndAssistant = cards.groupBy(SpreadSheetCardInfo::interviewer)
-            .mapValues { it.value.map { convertFromSheetCard(it) }.toMutableList()}.toMutableMap()
+            .mapValues { it.value.map {it -> it.toInterviewCardInfo() }.toMutableList()}.toMutableMap()
         cards.forEach {
             if (!it.assistant.isNullOrBlank()) {
-                val assistantCard = convertFromSheetCard(it)
+                val assistantCard = it.toInterviewCardInfo()
                 assistantCard.isAssistant = true
                 assistantCard.mainInterviewer = it.interviewer
                 cardsGroupedByInterviewerAndAssistant[it.assistant]?.add(assistantCard)
             }
         }
+        return cardsGroupedByInterviewerAndAssistant
     }
 }
