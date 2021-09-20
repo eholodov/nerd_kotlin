c5cd4971a3a11a023786cca08256ff004bc78199
Violetta Sytnikova <v.sytnikova@dunice.net>
2021-09-20 12:04:45 +0300

    Initial commit on validating

--- src/main/kotlin/com/dunice/nerd_kotlin/common/services/SlackService.kt
+++ src/main/kotlin/com/dunice/nerd_kotlin/common/services/SlackService.kt
@@ -1,10 +1,15 @@
 package com.dunice.nerd_kotlin.common.services
 
+import com.dunice.nerd_kotlin.common.utils.getNumberOfWeek
+import com.dunice.nerd_kotlin.common.utils.groupCardsByInterviewerAndAssistant
+import com.dunice.nerd_kotlin.common.utils.groupCardsByPerson
 import com.slack.api.Slack
 import com.slack.api.methods.request.chat.ChatPostMessageRequest
 import com.slack.api.methods.response.chat.ChatPostMessageResponse
+import org.springframework.stereotype.Service
 
-class SlackService {
+@Service
+class SlackService (val googleService: GoogleService) {
     var messageText : String = "Hello"
 
     var slack : Slack = Slack.getInstance()
@@ -20,5 +25,12 @@ class SlackService {
             .text(messageText)
             .build();
         var chatPostMessageResponse : ChatPostMessageResponse = methods.chatPostMessage(request)
+    }
+
+    fun validateFilterGroupUserList(
+    ) {
+        val sheetData = googleService.getInformation(getNumberOfWeek())
+        var employees = groupCardsByPerson(sheetData).keys + groupCardsByInterviewerAndAssistant(sheetData).keys
+
     }
 }
---
+++ src/main/kotlin/com/dunice/nerd_kotlin/common/types/UserListInfo.kt
@@ -0,0 +1,6 @@
+package com.dunice.nerd_kotlin.common.types
+
+data class UserListInfo(
+    val name: String,
+    val id: String
+)
