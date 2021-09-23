156f0de58b131765e807644402da7d3df0ddeb8e
Violetta Sytnikova <v.sytnikova@dunice.net>
2021-09-23 16:51:44 +0300

    Added error handling

--- src/main/kotlin/com/dunice/nerd_kotlin/common/errors/NerdControllerAdvice.kt
+++ src/main/kotlin/com/dunice/nerd_kotlin/common/errors/NerdControllerAdvice.kt
@@ -1,7 +1,13 @@
 package com.dunice.nerd_kotlin.common.errors
 
 import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
 class NerdControllerAdvice {
+
+    @ExceptionHandler(SlackEmailNotFoundException::class)
+    private fun handleEmailNotFoundException(ex: SlackEmailNotFoundException) {
+        println("🤢 EmailNotFound Exception is: ${ex.message} 🤢")
+    }
 }
