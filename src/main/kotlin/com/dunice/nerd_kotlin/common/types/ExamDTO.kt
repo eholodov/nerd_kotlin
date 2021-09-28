bcd6b95a370fbf6d37a0efab66f333f1f57eb75d
vsytnikova <v.sytnikova@dunice.net>
2021-09-28 11:32:10 +0300

    Added wrong email handling and fixed date parsing

--- src/main/kotlin/com/dunice/nerd_kotlin/common/types/ExamDTO.kt
+++ src/main/kotlin/com/dunice/nerd_kotlin/common/types/ExamDTO.kt
@@ -1,6 +1,9 @@
 package com.dunice.nerd_kotlin.common.types
 
 import com.dunice.nerd_kotlin.common.errors.*
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.Instant
 import java.time.OffsetDateTime
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
@@ -10,10 +13,12 @@ import javax.validation.constraints.Pattern
 data class ExamDTO(
 
     @field:NotNull(message = BLANK_DATETIME)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ssX")
     val datetime: OffsetDateTime,
 
     @field:Pattern(regexp = "[a-z]+(@dunice\\.net)", message = EMAIL_NOT_VALID_MESSAGE)
     @field:NotBlank(message = BLANK_EMAIL)
+    @JsonProperty("student_email")
     val studentEmail: String,
 
     @field:NotBlank(message = BLANK_SUBJECT)
@@ -24,8 +29,10 @@ data class ExamDTO(
 
     @field:Pattern(regexp = "[a-z]+(@dunice\\.net)", message = EMAIL_NOT_VALID_MESSAGE)
     @field:NotBlank(message = BLANK_INTERVIEWER)
+    @JsonProperty("interviewer_email")
     val interviewerEmail: String,
 
     @field:Pattern(regexp = "[a-z]+(@dunice\\.net)", message = EMAIL_NOT_VALID_MESSAGE)
+    @JsonProperty("assistant_email")
     val assistantEmail: String?
 )
