05f6e80de85266c680bcae139880d5163b304311
vsytnikova <84845358+vsytnikova@users.noreply.github.com>
2021-10-05 14:00:37 +0300

    Feature/token (#18)
    * Working on token
    
    * Working on token
    
    * Added token validation

---
+++ src/main/kotlin/com/dunice/nerd_kotlin/common/configs/SecurityConfig.kt
@@ -0,0 +1,32 @@
+package com.dunice.nerd_kotlin.common.configs
+
+import com.dunice.nerd_kotlin.common.jwt.JwtFilter
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(private val jwtFilter: JwtFilter) : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity?) {
+        http!!
+            .cors()
+            .and()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic()
+            .and()
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java);
+    }
+
+}
