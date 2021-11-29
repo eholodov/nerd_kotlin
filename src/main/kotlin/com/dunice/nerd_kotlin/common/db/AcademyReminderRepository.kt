4903fe9933a903252b76958bc2b60ac1feb5b53a
eholodov <e.holodov@dunice.net>
2021-11-29 09:53:56 +0300

    remove unused import

--- src/main/kotlin/com/dunice/nerd_kotlin/common/db/AcademyReminderRepository.kt
+++ src/main/kotlin/com/dunice/nerd_kotlin/common/db/AcademyReminderRepository.kt
@@ -1,7 +1,6 @@
 package com.dunice.nerd_kotlin.common.db
 
 import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.data.mongodb.repository.Query
 import org.springframework.stereotype.Repository
 import java.time.Instant
 
