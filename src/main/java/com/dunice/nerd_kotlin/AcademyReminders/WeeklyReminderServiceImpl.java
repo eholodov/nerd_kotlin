f292a9b0500960e440770d8683886b6195ea9b27
dimse <d.sevostyanov@dunice.net>
2022-01-19 12:31:57 +0300

    add import OffsetDateTime

--- src/main/java/com/dunice/nerd_kotlin/AcademyReminders/WeeklyReminderServiceImpl.java
+++ src/main/java/com/dunice/nerd_kotlin/AcademyReminders/WeeklyReminderServiceImpl.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.DayOfWeek;
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.WeekFields;
 import java.util.*;
